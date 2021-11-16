c0a227f4a7f18349dc850c906b9b963a7c27050f
Amory Galili <amorygalili@gmail.com>
2021-11-15 19:37:51 -0500

    Added counter component

---
+++ dashboard/src/component.js
@@ -0,0 +1,23 @@
+import { c, useProp } from "atomico";
+import html from "atomico/html";
+
+function myCounter() {
+  const [count, setCount] = useProp("count");
+  return html`
+    <host>
+      <button onclick=${() => setCount(count - 1)}>-</button>
+      <span>${count}</span>
+      <button onclick=${() => setCount(count + 1)}>+</button>
+    </host>
+  `;
+}
+
+myCounter.props = {
+  count: {
+    type: Number,
+    reflect: true,
+    value: 0
+  }
+};
+
+window.customElements.define("my-counter", c(myCounter));
